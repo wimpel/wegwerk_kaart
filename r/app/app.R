26052c114af5a8105103e9acf182ab21da2bd5df
wimbreedveld <wim.breedveld@gmail.com>
2022-06-24 09:51:58 +0200

    tijd selecteerbaar

--- r/app/app.R
+++ r/app/app.R
@@ -9,15 +9,19 @@ library(leaflet.extras2)
 library(sf)
 library(DT)
 library(shiny)
+library(shinyTime)
 
 routes <- readRDS("./data/routes.rds")
 werkzaamheden <- readRDS("./data/werkzaamheden.rds")
+#maak tijdstempels
+werkzaamheden[, `:=`(timestamp_van = as.POSIXct(paste0(van, " ", van_uur), tz = "Europe/Amsterdam"),
+                     timestamp_tot = as.POSIXct(paste0(tot, " ", tot_uur), tz = "Europe/Amsterdam"))]
+
 
 ### test
-# routes <- readRDS("./R/app/data/routes.rds")
-# werkzaamheden <- readRDS("./R/app/data/werkzaamheden.rds")
+#routes <- readRDS("./R/app/data/routes.rds")
+#werkzaamheden <- readRDS("./R/app/data/werkzaamheden.rds")
 ### /test
-
 
 ui <- (fluidPage(
   titlePanel("Stremmingen en omleidingen"),
@@ -25,8 +29,8 @@ ui <- (fluidPage(
     sidebarPanel(
       h2( "Selectie" ),
       # __user input -----
-      dateInput( 'dateInput',
-                 label     = "Datum", 
+      dateInput( 'dateInputVan',
+                 label     = "Van", 
                  value     = min(werkzaamheden$van, na.rm = TRUE),
                  min       = min(werkzaamheden$van, na.rm = TRUE),
                  max       = max(werkzaamheden$tot, na.rm = TRUE),
@@ -34,7 +38,27 @@ ui <- (fluidPage(
                  startview = "month",
                  weekstart = 1,
                  language  = "nl",
-                 autoclose = TRUE )
+                 autoclose = TRUE ),
+      timeInput( inputId      = "tijdInputVan",
+                 label        = "",
+                 value        = as.ITime("20:00:00"), 
+                 seconds      = FALSE,
+                 minute.steps = 30L ),
+      dateInput( 'dateInputTot',
+                 label     = "Tot", 
+                 value     = min(werkzaamheden$van, na.rm = TRUE) + 1,
+                 min       = min(werkzaamheden$van, na.rm = TRUE),
+                 max       = max(werkzaamheden$tot, na.rm = TRUE),
+                 format    = "dd-mm-yyyy",
+                 startview = "month",
+                 weekstart = 1,
+                 language  = "nl",
+                 autoclose = TRUE ),
+      timeInput( inputId      = "tijdInputTot",
+                 label        = "",
+                 value        = as.ITime("05:00:00"), 
+                 seconds      = FALSE,
+                 minute.steps = 30L ),
     ),
     mainPanel(
       tabsetPanel(
@@ -59,13 +83,36 @@ ui <- (fluidPage(
 server <- ( function( input, output, session ) {
 
   werk.sel <- reactive({
-    temp.sel <- werkzaamheden[van <= input$dateInput & tot >= input$dateInput, ]
+    #maak filterwaarden op basis van input
+    van.filter <- as.POSIXct(paste0(as.character(input$dateInputVan), "T", 
+                                    format(input$tijdInputVan, "%H:%M")), format = "%Y-%m-%dT%H:%M", 
+                             tz = "Europe/Amsterdam")
+    tot.filter <- as.POSIXct(paste0(as.character(input$dateInputTot), "T", 
+                                    format(input$tijdInputTot, "%H:%M")), format = "%Y-%m-%dT%H:%M", 
+                             tz = "Europe/Amsterdam")
+    temp.sel <- werkzaamheden[timestamp_van <= tot.filter & timestamp_tot >= van.filter, ]
     ### test
     #temp.sel <- werkzaamheden[van <= as.Date("2022-01-02") & tot >= as.Date("2022-01-02"), ]
     ### /test
     return(temp.sel)
   })
   
+  # Afdwingen dat eindtijd na de begintijd ligt
+  observeEvent({
+    input$dateInputVan 
+    input$dateInputTot
+    input$tijdInputVan
+    input$tijdInputTot
+    }, {
+      if (input$dateInputVan > input$dateInputTot) {
+        updateDateInput(session, "dateInputTot", value = input$dateInputVan + 1)
+        }
+      if (input$dateInputVan == input$dateInputTot & 
+          input$tijdInputVan >= input$tijdInputTot) {
+        updateTimeInput(session, "tijdInputTot", value = input$tijdInputVan + 1800)
+      }
+    })
+  # selecteer de werkzaamheden die binnen het opgegeven tijdvenster vallen
   route.sel <- reactive({
     req(werk.sel())
     temp.sel <- routes[routes$groep %in% unique(werk.sel()$groep), ]
--- r/test_werkzaamheden.R
+++ r/test_werkzaamheden.R
@@ -1,5 +1,5 @@
 #maak een dummy set met werkzaamheden
-
+library(data.table)
 df <- readRDS("./output/routes.rds")
 # werkzaamheden <- data.table(
 #   groep = unique(df$groep)[c(1,3)],
@@ -9,7 +9,9 @@ df <- readRDS("./output/routes.rds")
 werkzaamheden <- data.table(
   groep = unique(df$groep)[c(1,3)],
   van = seq(as.Date("2022-01-01", tz = "Europe/Amsterdam"), by = "3 days", length.out = 2),
-  tot = seq(as.Date("2022-01-07", tz = "Europe/Amsterdam"), by = "7 days", length.out = 2)
+  van_uur = c("22:00", "20:00"),
+  tot = seq(as.Date("2022-01-07", tz = "Europe/Amsterdam"), by = "7 days", length.out = 2),
+  tot_uur = c("03:00", "06:00")
 )
 
 saveRDS(werkzaamheden, "./output/werkzaamheden.rds")
